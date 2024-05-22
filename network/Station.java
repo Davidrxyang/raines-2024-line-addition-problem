f5a862a4467957e6b5331824d8bd6b784b2d8b57
davidrxy <davidrxyang@hotmail.com>
2024-05-22 17:05:36 -0400

    implemented station

--- network/Station.java
+++ network/Station.java
@@ -3,6 +3,26 @@
  */
 
 public class Station extends Object {
-    String name;
-    
+
+    String name; // full name of the station
+
+    /*
+     * geographical location of the station is stored using long/lat coordinates
+     * along with an address string
+     */
+
+    Double longitude; 
+    Double latitude;
+    String address;
+
+    String comment; 
+
+    public Station(String name, Double longitude, Double latitude) {
+        this.name = name;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.address = "";
+        this.comment = "";
+    }
 }
+
