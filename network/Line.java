3bfc86587d0929aa02bf6fa7c8b52d5150708adf
davidrxy <davidrxyang@hotmail.com>
2024-05-22 17:36:16 -0400

    started line

--- network/Line.java
+++ network/Line.java
@@ -1,7 +1,30 @@
 /*
  * a collection of connections that together represent one transit line 
+ * 
+ * each line is DIRECTED, since lines in reality go in both directions, 
+ * there will be two line instantiations for each line, one for each 
+ * direction
  */
 
+import java.util.ArrayList;
+
 public class Line {
-    
+    String name;
+    Station origin;
+    Station destination; // this is the direction of the line
+
+    ArrayList<Connection> connections;
+    // ArrayList<Station> stations; // will this be helpful?? 
+
+    public Line(String name) {
+        this.name = name;
+        connections = new ArrayList<>();
+    }
+
+    /*
+     * two methods to construct a line
+     * 
+     * 1. add a bunch of station IN ORDER - connections generated automatically
+     * 2. add a bunch of connections (unordered??) and specify origin and destination
+     */
 }
