58765acd24c8101fe9b906218cbd68fb043f33af
henrydeng2002 <henrydeng2002@gmail.com>
2024-05-27 17:34:54 -0400

    reverted to a previous version

--- WMATA-network/DemandSet.java
+++ WMATA-network/DemandSet.java
@@ -24,13 +24,14 @@ public class DemandSet {
         // load station information and number the stations
         stationNumber = new HashMap<Station, Integer>();
         // origin-destination matrix
-        demandMatrix = new int[network.stations.size()][network.stations.size()];
-        int i = 0;
-        ArrayList<Station> stationList = new ArrayList<Station>(network.stations.values());
-        for (Station station : stationList) {
-            stationNumber.put(station, i);
-            i++;
-        }
+
+        // demandMatrix = new int[network.stations.size()][network.stations.size()];
+        // int i = 0;
+        // ArrayList<Station> stationList = new ArrayList<Station>(network.stations.values());
+        // for (Station station : stationList) {
+        //     stationNumber.put(station, i);
+        //     i++;
+        // }
 
         // load the trips from the file
         try {
@@ -39,16 +40,30 @@ public class DemandSet {
             sc.nextLine();
             while (sc.hasNextLine()) {
                 String[] line = sc.nextLine().split(",");
-                String start = line[2];
-                String end = line[3];
-                int trips = Integer.parseInt(line[6]);
+                String start = line[2].substring(1, line[2].length() - 1);
+                String end = line[3].substring(1, line[3].length() - 1);
+                int trips = Integer.parseInt(line[6].substring(1, line[6].length() - 1));
 
-                Station startStation = network.stations.get(start);
-                Station endStation = network.stations.get(end);
+                // Station startStation = network.stations.get(start);
+                // Station endStation = network.stations.get(end);
+
+                System.out.println(start);
+                System.out.println(end);
+                System.out.println(trips);
             }
+
+            sc.close();
 
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
+    }
+
+    public static void main(String[] args) {
+
+        // sample usage
+        Network network = new Network();
+        DemandSet demandSet = new DemandSet();
+        demandSet.loadTrips("WMATA-network/data.csv", network);
     }
 }
