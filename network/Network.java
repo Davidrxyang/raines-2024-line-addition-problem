c6d097d35ed2cb7e8b36128df5b300189ce4b0f0
davidrxy <davidrxyang@hotmail.com>
2024-05-30 14:01:07 -0400

    network constructor and data members

--- network/Network.java
+++ network/Network.java
@@ -5,9 +5,43 @@
  */
 
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Network {
 
+    // nodes
+    public ArrayList<Station> stationList;
     public HashMap<String, Station> stations;
-    public HashMap<String, Line> lines;
+    int nStations;
+
+    // edges
+    public ArrayList<Connection> connections;
+
+    // lines
+    public ArrayList<Line> lines;
+    // public HashMap<String, Line> lines;
+
+    // adjacency matrix
+    public ArrayList< ArrayList< Double > > matrix;
+
+    public Network(ArrayList<Station> stationList) {
+        this.stationList = stationList;
+        nStations = stationList.size();
+        for (int i = 0; i < nStations; i++) {
+            stations.put(stationList.get(i).name, stationList.get(i));
+        }
+        connections = new ArrayList<>();
+        lines = new ArrayList<>();
+
+        // instantiate and populate the matrix
+        matrix = new ArrayList<>();
+
+        for (int i = 0; i < nStations; i++) {
+            matrix.add(new ArrayList<Double>(nStations));
+        }
+    }
+
+
+
+
 }
