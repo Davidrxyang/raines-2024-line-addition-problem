2f4144dbc5d07589561a10dda829e09062642a0a
henrydeng2002 <henrydeng2002@gmail.com>
2024-06-11 14:26:38 -0400

    initial RGA setup

--- existing-algorithms/RGA.java
+++ existing-algorithms/RGA.java
@@ -3,5 +3,36 @@
  */
 
 public class RGA {
+    // the demand of the network
+    public DemandSet l;
+
+    // the initial network containing station information
+    // and connectivity information
+    public Network existingNetwork;
+
+    // the network to be returned with transit lines
+    public Network R;
+
+    // the percentage of demand covered by the network
+    // D0: ratio of demand covered by network without transfers
+    // D01: ratio of demand covered by network with one transfer
+    public double D0 = 0;
+    public double D01 = 0;
+
+    // parameters to determine termination of the algorithm
+    // when the ratio of the demand covered by the network is higher than the minimums
+    public double D0min = 0.75;
+    public double D01min = 1;
+    public double totalTrips;
+
+    // number of initial skeletons
+    public int M;
     
+    public RGA(int M) {
+
+    }
+
+    public void removeSubsetRouts() {
+
+    }
 }
