ae146a80e2fbb366d281fca20ef4411b6cbdba41
davidrxyang <davidrxyang@hotmail.com>
2024-06-25 17:07:30 -0400

    CostData

---
+++ network-evaluation/Evaluation.java
@@ -0,0 +1,17 @@
+public class Evaluation {
+
+    CostData costData;
+
+    public Evaluation() {
+        costData = new CostData();
+    }
+
+    public Double ConstructionCost(Path path) {
+        Double cost = 0.0;
+        Double length = path.length;
+
+
+        return 0.0;
+
+    }
+}
