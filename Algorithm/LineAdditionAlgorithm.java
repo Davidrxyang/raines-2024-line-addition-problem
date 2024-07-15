cf551c2835424524adc320f8ec41a07c450fa637
henrydeng2002 <henrydeng2002@gmail.com>
2024-07-15 13:39:39 -0400

    made algorithm class

---
+++ Algorithm/LineAdditionAlgorithm.java
@@ -0,0 +1,20 @@
+package Algorithm;
+
+import javax.sound.sampled.Line;
+
+import Network.DemandSet;
+import Network.Network;
+import Network.Station;
+
+public class LineAdditionAlgorithm {
+    Network G;
+    DemandSet D;
+    
+    public LineAdditionAlgorithm(Network network, DemandSet demandSet, double targetEfficiency) {
+
+    }
+
+    public Line constructLine(Station vi, Station vj) {
+        return null;
+    }
+}
