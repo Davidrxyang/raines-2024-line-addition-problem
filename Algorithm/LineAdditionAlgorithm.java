b942791afd96ddb4333a41cbaefcd528a02a0358
henrydeng2002 <henrydeng2002@gmail.com>
2024-07-23 00:18:01 -0400

    sync with main

--- Algorithm/LineAdditionAlgorithm.java
+++ Algorithm/LineAdditionAlgorithm.java
@@ -59,7 +59,7 @@ public class LineAdditionAlgorithm {
             for (Line r_m : lineCandidates) {
                 if (r_m.stations.contains(v_i)) {
                     r_m_prime = addToLine(v_j, r_m);
-                    
+
                     if (r_m_prime != null) {
                         addLines.add(r_m_prime);
                         removeLines.add(r_m);
@@ -78,7 +78,7 @@ public class LineAdditionAlgorithm {
             for (Line r_n : lineCandidates) {
                 if (r_n.stations.contains(v_j)) {
                     r_n_prime = addToLine(v_i, r_n);
-                    
+
                     if (r_n_prime != null) {
                         addLines.add(r_n_prime);
                         removeLines.add(r_n);
@@ -173,22 +173,24 @@ public class LineAdditionAlgorithm {
     // optimization idea: affected path-aware paths
     // so that when one path is updated, only the affected paths are changed
     // ideally, we want to only recalculate modified demand for affected paths,
-    // however, due to the constantly changing nature of the network, subset-superset
-    // relationships are constantly changing and this efficiency improvement would 
+    // however, due to the constantly changing nature of the network,
+    // subset-superset
+    // relationships are constantly changing and this efficiency improvement would
     // introduce some inaccuracies in the calculation. The current comprehensive
-    // (but slow) approach exists for completeness sake. 
-    
-    public void updateEfficienciesAndDemand(Double c) {
+    // (but slow) approach exists for completeness sake.
+
+    public void updateEfficienciesAndDemand() {
         E = new PriorityQueue<>(); // reset E
         DemandSet modifiedDemand = new DemandSet();
         ArrayList<Path> paths = new ArrayList<>();
 
         // make a copy of the existing network and add the line candidates to it
         networkCopy = new Network(G);
-        for (int i = 0; i < lineCandidates.size(); i++){
+        for (int i = 0; i < lineCandidates.size(); i++) {
             lineCandidates.get(i).name = "candidate r" + i;
             networkCopy.addLine(lineCandidates.get(i));
-            networkCopy.addLine(lineCandidates.get(i).generateReverseDirection(lineCandidates.get(i).name + " reverse"));
+            networkCopy
+                    .addLine(lineCandidates.get(i).generateReverseDirection(lineCandidates.get(i).name + " reverse"));
         }
 
         // using astar for now: least transfers does not make sense for WMATA
